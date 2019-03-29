d2902f86bb70cc22a6d349c9521b699cba67dc4e
bogabi <bogdanbirgovan@gmail.com>
2019-03-29 15:35:13 +0100

    first step

--- dataproject/dataproject/__init__.py
+++ dataproject/dataproject/__init__.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 from pandas_datareader import wb
 import ipywidgets as widgets
 
+#GDP growth data from World Bank
+
+gdp = wb.download(indicator='NY.GDP.MKTP.KD.ZG', country=['all'], start=1997, end=2018)
+gdp = gdp.rename(columns = {'NY.GDP.MKTP.KD.ZG':'gdp growth'})
+gdp = gdp.reset_index()
+gdp['year']=gdp['year'].astype(int) #datetime %year%
+print(gdp)
+
